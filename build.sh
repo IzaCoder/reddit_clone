b549816917027e5f48e9efcb4f331914e18ea31c
IzaCoder <izacoder@icloud.com>
2022-01-02 02:28:24 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,2 @@
 export PATH="./dart-sass:$PATH"
 sass scss/styles.scss styles.min.css --style compressed
-git config --global user.name "IzaCoder"
-git config --global user.email "izacoder@icloud.com"
-git add styles.min.css
-git commit -m "Compile & minify SCSS"
-git branch -M main
-git push origin main
