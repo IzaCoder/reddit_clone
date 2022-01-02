eeb361f583b757fc7ee5f2ae81c798977cb3d312
IzaCoder <izacoder@icloud.com>
2022-01-02 18:44:32 +0000

    Add password-confirmation logic

--- create_account.js
+++ create_account.js
@@ -4,4 +4,9 @@ accountCreationForm.onsubmit = () =>
     const username = document.getElementById("username");
     const password = document.getElementById("password");
     const passwordConfirm = document.getElementById("password-confirm");
+
+    if (password !== passwordConfirm) {
+        document.getElementById("error-messages").innerHTML += "Make sure the re-type password field is the same as the password field\n";
+        return false;
+    }
 }
