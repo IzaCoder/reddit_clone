56ea7074a37d27c149686f53b66304a6b3217f6b
IzaCoder <izacoder@icloud.com>
2022-01-02 18:41:27 +0000

    Declare password constant

--- create_account.js
+++ create_account.js
@@ -2,5 +2,6 @@ const accountCreationForm = document.getElementById("create-account");
 
 accountCreationForm.onsubmit = () => 
     const username = document.getElementById("username");
+    const password = document.getElementById("password");
     
 }
