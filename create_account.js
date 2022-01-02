7a6d77bdc7ea10bdcfe97b010eff3929e47d8b09
IzaCoder <izacoder@icloud.com>
2022-01-02 18:54:12 +0000

    Add debugging alert (please please work this time) :

--- create_account.js
+++ create_account.js
@@ -4,6 +4,7 @@ accountCreationForm.onsubmit = (event) =>
     const username = document.getElementById("username");
     const password = document.getElementById("password").value;
     const passwordConfirm = document.getElementById("password-confirm").value;
+    alert(password + " " + passwordConfirm);
 
     if (password !== passwordConfirm) {
         document.getElementById("error-messages").innerHTML += "Make sure the re-type password field is the same as the password field\n";
