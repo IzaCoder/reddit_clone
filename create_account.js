68b684f019bc2b80b906f7ee8d74d2c06691d99e
IzaCoder <izacoder@icloud.com>
2022-01-04 21:39:40 +0000

    Add event handler for form submission

--- create_account.js
+++ create_account.js
@@ -1,3 +1,7 @@
 const accountCreationForm = document.getElementById("create-account");
 
 alert(accountCreationForm)
+
+accountCreationForm.onsubmit = function () {
+    alert("You submitted the form!")
+}
