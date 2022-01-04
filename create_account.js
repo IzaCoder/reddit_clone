5fa00023f544694d318742cd3e02fd47a57c5a57
IzaCoder <izacoder@icloud.com>
2022-01-04 21:00:34 +0000

    Remove form submission event handler

--- create_account.js
+++ create_account.js
@@ -1,12 +1,1 @@
 const accountCreationForm = document.getElementById("create-account");
-
-accountCreationForm.onsubmit = (event) => 
-    const username = document.getElementById("username");
-    const password = document.getElementById("password").value;
-    const passwordConfirm = document.getElementById("password-confirm").value;
-
-    if (password !== passwordConfirm) {
-        document.getElementById("error-messages").innerHTML += "Make sure the re-type password field is the same as the password field\n";
-        event.preventDefault();
-    }
-}
